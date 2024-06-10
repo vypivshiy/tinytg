e7d67528e4bb440059f36168ca125cba5aaf43ce
georgiy <59173419+vypivshiy@users.noreply.github.com>
2024-06-10 14:16:21 +0300

    refactoring, decrease lines of code (breaking the pep8)

--- tinytg.py
+++ tinytg.py
@@ -1,17 +1,11 @@
 """
-Copyright (c) 2024 vypivshiy
-this code under the UNLICENSE or MIT License.
+Copyright (c) 2024 vypivshiy this code under the UNLICENSE or MIT License.
+source: https://github.com/vypivshiy/tinytg
 
 request code under the UNLICENSE or MIT License.
-author: sesh
 source: https://github.com/sesh/thttp
 """
-import gzip
-import json as json_lib
-import logging
-import mimetypes
-import re
-import ssl
+import gzip, logging, mimetypes, re, ssl, json as json_lib
 from base64 import b64encode
 from collections import namedtuple
 from http.cookiejar import CookieJar
@@ -44,27 +38,18 @@ def request(method, url, params=None, json=None, data=None, headers=None,
            - url (final url, after any redirects)
            - cookiejar
     """
-    params = params or {}
-    headers = headers or {}
-    files = files or {}
-    method = method.upper()
+    params, headers, files, method = params or {}, headers or {}, files or {}, method.upper()
     headers = {k.lower(): v for k, v in headers.items()}
+    url += f"?{urlencode(params)}" if params else ""
 
     if method not in {"POST", "PATCH", "PUT", "GET"}:
         raise ValueError("Unknown method type")
-
-    if params:
-        url += f"?{urlencode(params)}"
-
     if json and data:
         raise ValueError("Cannot provide both json and data parameters")
-
     if method not in ["POST", "PATCH", "PUT"] and (json or data):
         raise ValueError("Request method must be POST, PATCH or PUT if json or data is provided")
-
     if files and method != "POST":
         raise ValueError("Request method must be POST when uploading files")
-
     if json:
         headers["content-type"] = "application/json"
         data = json_lib.dumps(json).encode("utf-8")
@@ -75,16 +60,15 @@ def request(method, url, params=None, json=None, data=None, headers=None,
             data = data.encode()
     elif files:
         data, boundary = _encode_multipart_formdata(files)
-        headers["Content-Type"] = f"multipart/form-data; boundary={boundary}"
-        headers["Content-Length"] = str(len(data))
+        headers["content-type"] = f"multipart/form-data; boundary={boundary}"
+        headers["content-length"] = str(len(data))
 
     if basic_auth and len(basic_auth) == 2 and "authorization" not in headers:
         username, password = basic_auth
         auth_value = b64encode(f"{username}:{password}".encode()).decode("ascii")
         headers["authorization"] = f"Basic {auth_value}"
 
-    if cookiejar is None:
-        cookiejar = CookieJar()
+    cookiejar = cookiejar or CookieJar()
 
     ctx = ssl.create_default_context()
     if not verify:
@@ -106,41 +90,36 @@ def request(method, url, params=None, json=None, data=None, headers=None,
 
 
 def _encode_multipart_formdata(files):
-    boundary = str(uuid4())
-    data = b""
-
+    boundary, lines = str(uuid4()).encode(), []
     for key, file in files.items():
         file_data = file.read()
-        if isinstance(file_data, str):
-            file_data = file_data.encode("utf-8")
-        filename = file.name
-        mime_type = mimetypes.guess_type(filename)[0] or "application/octet-stream"
-
-        data += (b"--" + boundary.encode() + b"\r\n"
-                                             b'Content-Disposition: form-data; name="' + key.encode() + b'"; filename="' + filename.encode() + b'"\r\n'
-                                                                                                                                              b"Content-Type: " + mime_type.encode() + b"\r\n\r\n"
-                + file_data + b"\r\n"
-        )
-
-    data += b"--" + boundary.encode() + b"--\r\n"
-    return data, boundary
+        file_data = file_data.encode("utf-8") if isinstance(file_data, str) else file_data
+        filename = file.name.encode()
+        mime_type = mimetypes.guess_type(filename.decode())[0] or "application/octet-stream"
+        lines.extend([
+            b"--" + boundary,
+            b'Content-Disposition: form-data; name="' + key.encode() + b'"; filename="' + filename + b'"',
+            b"Content-Type: " + mime_type.encode(),
+            b"",
+            file_data,
+            *[b"--" + boundary + b"--", b""]
+        ])
+    return b"\r\n".join(lines), boundary.decode()
 
 
 def _parse_response(response, ulib_request, cookiejar, is_error=False):
-    status = response.code if is_error else response.getcode()
-    content = response.read()
-    headers = {k.lower(): v for k, v in response.info().items()}
+    status, content, headers = (response.code if is_error else response.getcode(), response.read(),
+                                {k.lower(): v for k, v in response.info().items()})
+    content = gzip.decompress(content) if "gzip" in headers.get("content-encoding", "") else content
+    json_content =  json_lib.loads(content) if "application/json" in headers.get("content-type", "").lower() and content else None
+    return Response(request=ulib_request, content=content, json=json_content, status=status, url=response.geturl(), 
+                    headers=headers, cookiejar=cookiejar)
 
-    if "gzip" in headers.get("content-encoding", ""):
-        content = gzip.decompress(content)
 
-    json_content = None
-    if "application/json" in headers.get("content-type", "").lower() and content:
-        json_content = json_lib.loads(content)
-
-    return Response(request=ulib_request, content=content, json=json_content, status=status,
-                    url=response.geturl(), headers=headers, cookiejar=cookiejar)
-
+def read_env(env_file='.env'):
+    """simple env files reader"""
+    with open(env_file, 'r') as f:
+        return {k.strip(): v.strip() for k, v in (line.split('=', 1) for line in f if line.strip() and not line.strip().startswith('#'))}
 
 # telegram event types
 class Document(NamedTuple):
@@ -149,23 +128,17 @@ class Document(NamedTuple):
     file_id: str
     file_unique_id: str
     file_size: int
-
-
 class FromUser(NamedTuple):
     id: int
     is_bot: bool
     first_name: str
     language_code: str
     username: Optional[str] = None
-
-
 class Chat(NamedTuple):
     id: int
     type: str
     first_name: str
     username: Optional[str] = None
-
-
 class Message(NamedTuple):
     message_id: int
     from_: FromUser
@@ -174,37 +147,29 @@ class Message(NamedTuple):
     text: Optional[str] = None
     entities: Optional[List] = []
     document: Optional[Document] = None
-
-
 class MessageEvent(NamedTuple):
     update_id: int
     message: Message
-
-
+# typing
 T_RULE = Callable[[Message], bool]
 T_RULES = Tuple[T_RULE, ...]
 T_MSG_EVENT = Callable[[Message, ...], None]
 T_PARSE_ARGS_CB = Callable[[Message], Tuple[Any, ...]]
 T_CALLBACKS = List[Tuple[T_MSG_EVENT, T_RULES, T_PARSE_ARGS_CB]]
-
-
-def read_env(env_file='.env'):
-    """simple env files reader"""
-    with open(env_file, 'r') as f:
-        return {k.strip(): v.strip() for k, v in
-                (line.split('=', 1) for line in f if line.strip() and not line.strip().startswith('#'))}
-
-
+# rules shortcuts
 F_IS_BOT = lambda m: m.from_.is_bot == True
 F_IS_USER = lambda m: m.from_.is_bot == False
 F_ALLOW_USERS = lambda *user_ids: lambda m: m.from_.id in user_ids
 F_COMMAND = lambda command: lambda m: bool(m.text) and bool(re.match(command, m.text)) and m.from_.is_bot == False
 F_RE = lambda pattern: lambda m: bool(m.text) and re.search(pattern, m.text)
 F_IS_ATTACHMENT = lambda m: bool(m.document)
-
-logging.basicConfig(format='%(asctime)s [%(levelname)s] %(name)s: %(message)s',
-                    level=getattr(logging, read_env().get('LOG_LEVEL', 'DEBUG')))
-
+try:
+    _lvl = read_env('.env').get('LOG_LEVEL', 'DEBUG')
+except Exception as _:
+    logging.warning('failed read "LOG_LEVEL" key in .env file. set DEBUG mode')
+    _lvl = logging.DEBUG
+logging.basicConfig(format='%(asctime)s [%(levelname)s] %(name)s: %(message)s', level=_lvl)
+logger = logging.getLogger()
 
 class API:
     BASE_URL = 'https://api.telegram.org/bot{}'
@@ -227,52 +192,43 @@ class API:
             try:
                 resp = self.api_request(method, api_method, **data)
                 if (result := resp.json.get('result')) and result:
-                    logging.debug('%s %s', resp.status, resp.json)
+                    logger.debug('%s %s', resp.status, resp.json)
                 return resp
             except Exception as e:
-                logging.exception('ERROR: %s %s', api_method, e)
+                logger.exception('ERROR: %s %s', api_method, e)
                 if i == max_tries:
                     raise e
                 sleep(1)
 
     def get_updates(self, offset: Optional[int] = None):
-        return self.try_send_api_request('POST', 'getUpdates',
-                                         timeout=30, data={'offset': offset}).json['result']
+        return self.try_send_api_request('POST', 'getUpdates', timeout=30, data={'offset': offset}).json['result']
 
     def send_message(self, chat_id: int, text: str):
         return self.try_send_api_request('POST', 'sendMessage', data={'chat_id': chat_id, 'text': text})
 
     def reply_message(self, chat_id: int, message_id: int, text: str):
-        return self.try_send_api_request('POST', 'sendMessage',
-                                         data={'chat_id': chat_id, 'text': text, 'reply_to_message_id': message_id})
+        return self.try_send_api_request('POST', 'sendMessage', data={'chat_id': chat_id, 'text': text, 'reply_to_message_id': message_id})
 
     def send_document(self, file_ctx: BinaryIO, chat_id: int):
         # this api files sent cannot provide normal data form handling. send it as a params form
-        return self.try_send_api_request(
-            'POST', 'sendDocument', files={'document': file_ctx}, params={'chat_id': chat_id})
+        return self.try_send_api_request('POST', 'sendDocument', files={'document': file_ctx}, params={'chat_id': chat_id})
 
     def send_photo(self, file_ctx: BinaryIO, chat_id: int):
-        return self.try_send_api_request(
-            'POST', 'sendPhoto', files={'photo': file_ctx}, params={'chat_id': chat_id})
+        return self.try_send_api_request('POST', 'sendPhoto', files={'photo': file_ctx}, params={'chat_id': chat_id})
 
     def send_audio(self, file_ctx: BinaryIO, chat_id: int):
-        return self.try_send_api_request(
-            'POST', 'sendAudio', files={'audio': file_ctx}, params={'chat_id': chat_id})
+        return self.try_send_api_request('POST', 'sendAudio', files={'audio': file_ctx}, params={'chat_id': chat_id})
 
     def send_video(self, file_ctx: BinaryIO, chat_id: int):
-        return self.try_send_api_request(
-            'POST', 'sendVideo', files={'video': file_ctx}, params={'chat_id': chat_id})
+        return self.try_send_api_request('POST', 'sendVideo', files={'video': file_ctx}, params={'chat_id': chat_id})
 
     def send_voice(self, file_ctx: BinaryIO, chat_id: int):
-        return self.try_send_api_request(
-            'POST', 'sendVoice', files={'voice': file_ctx}, params={'chat_id': chat_id})
+        return self.try_send_api_request('POST', 'sendVoice', files={'voice': file_ctx}, params={'chat_id': chat_id})
 
 
 class Bot:
-    POLLING_INTERVAL = 1.0
-
     def __init__(self, token: str = read_env()["TOKEN"],
-                 polling_interval=read_env().get("POLLING_INTERVAL", '1.0'),
+                 polling_interval: float =float(read_env().get("POLLING_INTERVAL", '1.0')),
                  global_rules: Iterable[T_RULE] = ()):
         """main bot instance
 
@@ -282,29 +238,26 @@ class Bot:
         """
         self._callbacks: T_CALLBACKS = []
         self._api = API(token)
-        self.POLLING_INTERVAL = float(polling_interval)
+        self.POLLING_INTERVAL = polling_interval
         self._global_rules = global_rules
 
     @property
-    def api(self) -> API:
-        return self._api
+    def api(self) -> API: return self._api
 
     @staticmethod
     def _parse_msg_event(msg: dict) -> MessageEvent:
         doc = Document(**msg['message']['document']) if msg['message'].get('document') else None
-        return MessageEvent(
-            update_id=msg['update_id'],
-            message=Message(message_id=msg['message']['message_id'],
+        return MessageEvent(update_id=msg['update_id'],
+                            message=Message(message_id=msg['message']['message_id'],
                             from_=FromUser(**msg['message']['from']),
                             date=msg['message']['date'],
                             text=msg['message'].get('text', None),
                             entities=msg['message'].get('entities', []),
                             chat=Chat(**msg['message']['chat']),
-                            document=doc)
-        )
+                            document=doc))
 
     def polling(self):
-        last_update_id = None
+        last_update_id, _ = None, logger.debug('start bot')
         while True:
             try:
                 for update in self.api.get_updates(last_update_id):
@@ -313,7 +266,7 @@ class Bot:
                         self._handle_callback(event.message)
                         last_update_id = event.update_id + 1
             except Exception as e:
-                logging.exception(e)
+                logger.exception(e)
             sleep(self.POLLING_INTERVAL)
 
     @staticmethod
@@ -323,7 +276,7 @@ class Bot:
                 if not rule(m):
                     return False
             except Exception as e:
-                logging.exception('Rule throw exc %s', e)
+                logger.exception('Rule throw exc %s', e)
         return True
 
     def _handle_callback(self, message: Message) -> None:
@@ -332,24 +285,16 @@ class Bot:
                 try:
                     args = parse_cb(message)
                 except Exception as e:
-                    logging.error('failed extract arguments')
-                    logging.exception("%s", e)
+                    logger.error('failed extract arguments'), logger.exception("%s", e)
                     args = ()
-                logging.debug('handling "%s" with args: %s', cb.__name__, args)
-                cb(message, *args)
+                logger.debug('handling "%s" with args: %s', cb.__name__, args), cb(message, *args)
 
-    def on_message(self,
-                   *rules: T_RULE,
-                   parse_cb: T_PARSE_ARGS_CB = lambda m: (),
-                   ):
+    def on_message(self, *rules: T_RULE, parse_cb: T_PARSE_ARGS_CB = lambda m: ()):
         """base message event handler decorator
 
         :param parse_cb: optional parse arguments callback from message
         :param rules: event activate rules
-        :return:
         """
-
         def decorator(callback: T_MSG_EVENT) -> None:
             self._callbacks.append((callback, rules, parse_cb))
-
         return decorator
